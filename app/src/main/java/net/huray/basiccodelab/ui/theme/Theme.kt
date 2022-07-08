eed53108bdaa755df6f513a708eb0d554407400b
Woogear Kwon <techkwon1717@hanmail.net>
2022-07-08 15:55:53 +0900

    add theme components

--- app/src/main/java/net/huray/basiccodelab/ui/theme/Theme.kt
+++ app/src/main/java/net/huray/basiccodelab/ui/theme/Theme.kt
@@ -30,6 +30,27 @@ private val LightColorPalette = lightColors(
     */
 )
 
+private val LightThemeColors = lightColors(
+    primary = Red700,
+    primaryVariant = Red900,
+    onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800,
+    onBackground = Color.Black
+)
+
+private val DarkThemeColors = darkColors(
+    primary = Red300,
+    primaryVariant = Red700,
+    onPrimary = Color.Black,
+    secondary = Red300,
+    onSecondary = Color.Black,
+    error = Red200,
+    onBackground = Color.White
+)
+
 @Composable
 fun BasicCodelabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
